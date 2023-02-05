755840f9de17c51e8fed031dfabe5994a80bef0f
nguyenminhduc2c19 <nguyenminhduc2c19@gmail.com>
2023-02-06 00:04:09 +0700

    hoan thanh trang chu

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function openMenu(){
-    const status=true;
-    if(status){
-        
-    }
+const addingPostNote = document.querySelector('#blabla');
+const add_icon = document.querySelector('.add-icon');
+const hamburger_button = document.querySelector('.hamburger-button');
+const function_items= document.querySelector('.functions');
+add_icon.addEventListener("mouseover",function(){
+    
+    addingPostNote.classList.remove('hidden');
+})
+add_icon.addEventListener("mouseout",function(){
+    addingPostNote.classList.add('hidden');
+})
+hamburger_button.addEventListener("click",function(){
+if(function_items.style.display==='none'){
+    function_items.style.display='block'
+}else{
+    function_items.style.display='none'
 }
+})
