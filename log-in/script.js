50843420b2ec6df98066fd207cb50a983be7aaae
nguyenminhduc2c19 <nguyenminhduc2c19@gmail.com>
2023-02-09 17:53:55 +0700

    basic_inteface_about_log_in

---
+++ log-in/script.js
@@ -0,0 +1,5 @@
+const log_in = document.querySelector('.submit');
+log_in.addEventListener('click',function(event){
+event.preventDefault();
+window.location.href='http://127.0.0.1:5500/homepage.html'
+})
